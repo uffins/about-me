3f58d6cda492efdd3688fd54e26b139204436a22
유효경 <hyogyeong.y@ingstory.kr>
2022-08-29 18:27:47 +0900

    기본 profile 추가

--- src/App.js
+++ src/App.js
@@ -1,25 +1,34 @@
-import logo from './logo.png';
-import './App.css';
+import React, { useState } from 'react';
+import {Col, Container, Image, Row} from 'react-bootstrap';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const App = () => (
+  <div className="App">
+    <Container fluid="sm" className="p-3">
+      <Row>
+        <div className="App-header">
+          <h1 className="display-2 fw-bold">ABOUT;</h1>
+          <strong>HyoGyeong Ryu</strong>
+        </div>
+        <hr/>
+      </Row>
+      <Row className="mt-4">
+        <Col xs="auto">
+          <Image roundedCircle="true"
+                 className="border border-success border-5"
+                 src="/profile.jpg"
+                 alt="photo" />
+        </Col>
+        <Col>
+            <h2 className="fw-bold">Profile</h2>
+            <ul className="lh-lg list-unstyled mt-2">
+              <li>🎂 1992. 06. 16.</li>
+              <li>💌 fromben@kakao.com</li>
+              <li>🏠 서울시 서초구 방배동</li>
+            </ul>
+        </Col>
+      </Row>
+    </Container>
+  </div>
+);
 
 export default App;
